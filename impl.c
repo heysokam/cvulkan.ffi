80ffdacc6a61b84926ceea3a3f93f9df03059056
sOkam <7308253+heysokam@users.noreply.github.com>
2025-07-21 09:04:45 +0200

    new: cvulkan.zig codegen from cvulkan root buildsystem

---
+++ impl.c
@@ -0,0 +1,8 @@
+//:___________________________________________________________
+//  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
+//:___________________________________________________________
+//! @fileoverview
+//!  Implementation file for the Zig/Nim binding generators.
+//___________________________________________________________|
+#define cvk_Implementation
+#include "../cvulkan.h"
