9e97c0bf03ab6a6c27d674fbff759efb5b13438e
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-05 09:50:30 +0200

    chg: Generate Nim FFI without renames
    Will create the nimmified API manually instead. Its less work.

--- gen_raw.nim
+++
@@ -1,9 +0,0 @@
-#:___________________________________________________________
-#  cvulkan  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
-#:___________________________________________________________
-import std/os
-import futhark
-importc:
-  path "../"
-  outputPath currentSourcePath.parentDir/"nim"/"src"/"cvulkan"/"raw.nim"
-  "cvulkan.h"
